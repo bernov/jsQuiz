9d765c48629693e8798eb5c8c262fa7c138eb703
bernov <bernov@gmail.com>
2020-09-12 16:53:53 +0600

    final commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+const showResult = document.querySelector('.result');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -18,6 +19,36 @@ form.addEventListener('submit', (e) => {
       score += 25;
     }
   });
+  // console.log(score);
+  // show result of poll
+  scrollTo(0, 0);
+  // showResult.querySelector('span').textContent = `${score}%`;
+  showResult.classList.remove('d-none');
 
-  console.log(score);
+  let output = 0;
+  const timer = setInterval(() => {
+    showResult.querySelector('span').textContent = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
 });
+
+// console.log('hello');
+// document.querySelector('form');
+// console.log(form);
+// setTimeout(() => {
+//   alert('hello ninjas');
+// }, 5000);
+// setInterval
+
+// let i = 0;
+// const timer = setInterval(() => {
+//   console.log('hello');
+//   i++;
+//   if (i === 5) {
+//     clearInterval(timer);
+//   }
+// }, 1000);
